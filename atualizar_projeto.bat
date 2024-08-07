3249cac0f526c8305f46524f01545cfc02b26673
Marcos Silva <marcossilva8428@gmail.com>
2024-08-07 19:15:44 -0300

    Atualizando projeto javaEssentials com as ultimas mudancas

--- atualizar_projeto.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git config --global --add safe.directory C:/Users/marcos.da.silva.j/eclipse-workspace/java
-cd C:/Users/marcos.da.silva.j/eclipse-workspace/java
-xcopy C:/Users/marcos.da.silva.j/eclipse-workspace/javaEssentials/* javaEssentials/ /E /I
-git add .
-git commit -m "Atualizando projeto javaEssentials com as últimas mudanças"
-git push origin main
-pause
