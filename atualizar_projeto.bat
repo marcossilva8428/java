c0a97d55dc5d9b7d39778c321541a27621dc73dd
Marcos Silva <marcossilva8428@gmail.com>
2024-08-08 20:27:20 -0300

    Atualizando projeto javaEssentials com as ultimas mudancas

--- atualizar_projeto.bat
+++ atualizar_projeto.bat
@@ -1,8 +1,20 @@
 @echo off
+REM Configura o diretório como seguro para o Git
 git config --global --add safe.directory C:/Users/marcos.da.silva.j/eclipse-workspace/java
+
+REM Navega até o diretório do projeto
 cd C:/Users/marcos.da.silva.j/eclipse-workspace/java
+
+REM Copia os arquivos do diretório javaEssentials
 xcopy C:/Users/marcos.da.silva.j/eclipse-workspace/javaEssentials/* javaEssentials/ /E /I
+
+REM Adiciona os arquivos ao Git
 git add .
+
+REM Confirma as mudanças com uma mensagem de commit
 git commit -m "Atualizando projeto javaEssentials com as ultimas mudancas"
+
+REM Envia as mudanças para o repositório remoto
 git push origin main
+
 pause
